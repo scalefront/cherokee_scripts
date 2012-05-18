29d464490852ed619070a50a53863d183ed86f38
Brian McDonnell <brian@scalefront.com>
2012-05-18 16:24:35 +0100

    Script converts up/down argument to enable/disable

--- maintenance.sh
+++ maintenance.sh
@@ -4,15 +4,23 @@
 # the maintenance.py script and reload cherokee web
 # server.
 #
-# This script takes enable/disable as the first argument
+# This script takes enable/disable or up/down as the first argument
 
 if [ "$(whoami)" != "root" ]; then
     echo "This script needs to be run as sudo/root."
     exit 1
 fi
 
+# Convert up/down to enable/disable
+action=$1
+if [ $action == 'up' ]; then
+    action='enable'
+elif [ $action == 'down' ]; then
+    action='disable'
+fi
+
 # Specify the vserver nick that should be affected.
-./maintenance.py "$1" --vserver_nick="Example Nick"
+./maintenance.py "$action" --vserver_nick="Example Nick"
 
 # Only reload cherokee if the maintenance script succeeded
 if [ "$?" == 0 ]; then
